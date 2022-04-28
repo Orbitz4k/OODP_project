d52302c21b55473d9449d722a40f7222f5a4f35b
Orbitz4k <B00124102@mytudublin.ie>
2022-04-28 14:51:43 +0100

    Added site functionality

--- projectStarter/src/DataFixtures/AppFixtures.php
+++ projectStarter/src/DataFixtures/AppFixtures.php
@@ -8,11 +8,26 @@ use Doctrine\Persistence\ObjectManager;
 use App\Factory\UserFactory;
 use App\Factory\MakeFactory;
 use App\Factory\PhoneFactory;
+use App\Entity\Solicitors;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $solicitor = new Solicitors();
+        $solicitor->setName("Matt Murdock");
+        $solicitor->setClients("1000");
+        $solicitor->setCourts("New York");
+        $manager->persist($solicitor);
+        $manager->flush();
+
+        $solicitor = new Solicitors();
+        $solicitor->setName("Foggy Nelson");
+        $solicitor->setClients("1001");
+        $solicitor->setCourts("New York");
+        $manager->persist($solicitor);
+        $manager->flush();
+
         UserFactory::createOne([
             'username' => 'matt',
             'password' => 'smith',
@@ -24,6 +39,5 @@ class AppFixtures extends Fixture
             'password' => 'doe',
             'role' => 'ROLE_ADMIN'
         ]);
-
     }
 }
